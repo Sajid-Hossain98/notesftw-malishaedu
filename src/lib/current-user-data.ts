c0feb88cb68c6878feae05f6356e8f459474fd6f
Sajid Hossain <bmsajidhossain420@gmail.com>
2024-12-15 20:59:47 +0600

    lib to get current user data from database

--- src/lib/current-user-data.ts
+++ src/lib/current-user-data.ts
@@ -1,18 +1,18 @@
 import { auth } from "@clerk/nextjs/server";
 import { db } from "./db";
 
-export const currentUser = async () => {
+export const currentUserData = async () => {
   const { userId } = await auth();
 
   if (!userId) {
     return null;
   }
 
-  const currentUser = await db.user.findUnique({
+  const currentUserData = await db.user.findUnique({
     where: {
       clerkUserId: userId,
     },
   });
 
-  return currentUser;
+  return currentUserData;
 };
