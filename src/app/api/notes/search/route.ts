e6e532ae9f14841454f0ac5698e734974bc8f9bb
Sajid Hossain <bmsajidhossain420@gmail.com>
2025-01-05 20:03:39 +0600

    api route to get search data

---
+++ src/app/api/notes/search/route.ts
@@ -0,0 +1,63 @@
+import { db } from "@/lib/db";
+import { Note } from "@prisma/client";
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  // Getting the search query parameter
+  const url = new URL(request.url);
+  const searchedUniversity = url.searchParams.get("university") || "";
+
+  if (!searchedUniversity) {
+    return NextResponse.json([], { status: 400 });
+  }
+
+  try {
+    const notes = await db.note.findMany({
+      where: {
+        OR: [
+          {
+            university: {
+              universityFullName: {
+                contains: searchedUniversity,
+                mode: "insensitive",
+              },
+            },
+          },
+          {
+            university: {
+              universityShortName: {
+                contains: searchedUniversity,
+                mode: "insensitive",
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        university: true,
+        type: true,
+      },
+    });
+
+    const groupedNotes = notes.reduce((acc: Record<string, Note[]>, note) => {
+      const typeName = note.type.name;
+
+      if (!acc[typeName]) {
+        acc[typeName] = [];
+      }
+
+      acc[typeName].push(note);
+
+      return acc;
+    }, {});
+
+    return NextResponse.json(groupedNotes);
+  } catch (error) {
+    console.log(error);
+
+    return NextResponse.json(
+      { error: "Error fetching searched notes" },
+      { status: 500 }
+    );
+  }
+}
