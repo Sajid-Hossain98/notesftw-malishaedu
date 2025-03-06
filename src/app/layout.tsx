1dc5e9a8f6e100e0f83095d4dd3ac54fd44f7996
Sajid Hossain <bmsajidhossain420@gmail.com>
2025-03-07 01:15:10 +0600

    fix: favicon[wrong path]

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -40,7 +40,7 @@ export const metadata: Metadata = {
   //   "minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover",
   icons: [
     { rel: "apple-touch-icon", url: "icons/Logo-128.png" },
-    { rel: "icon", url: "icons/icon-128x128.png" },
+    { rel: "icon", url: "icons/Logo-128.png" },
   ],
 };
 
