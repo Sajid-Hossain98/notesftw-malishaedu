3cdb87452da856b7cd12280a1b4f06daa6f1292f
Sajid Hossain <bmsajidhossain420@gmail.com>
2025-10-18 10:30:46 +0600

    Syncing role in clerk metadata  with DB

--- src/lib/initialUser.ts
+++ src/lib/initialUser.ts
@@ -1,4 +1,4 @@
-import { currentUser } from "@clerk/nextjs/server";
+import { currentUser, clerkClient } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 import { db } from "@/lib/db";
@@ -12,11 +12,21 @@ export const initialUser = async () => {
 
   const existingUser = await db.user.findUnique({
     where: {
-      clerkUserId: user?.id,
+      clerkUserId: user.id,
     },
   });
 
   if (existingUser) {
+    const clerkRole = user.publicMetadata?.role;
+
+    if (clerkRole !== existingUser.role) {
+      (await clerkClient()).users.updateUserMetadata(user.id, {
+        publicMetadata: {
+          role: existingUser.role,
+        },
+      });
+    }
+
     return existingUser;
   }
 
@@ -26,6 +36,12 @@ export const initialUser = async () => {
       name: `${user?.firstName} ${user?.lastName}`,
       imageUrl: user?.imageUrl,
       email: user?.emailAddresses[0].emailAddress,
+    },
+  });
+
+  (await clerkClient()).users.updateUserMetadata(user.id, {
+    publicMetadata: {
+      role: "USER",
     },
   });
 
