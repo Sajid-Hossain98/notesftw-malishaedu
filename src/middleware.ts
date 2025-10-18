aaf665588dff46d439e4898d94899fe6356f9efe
Sajid Hossain <bmsajidhossain420@gmail.com>
2025-10-18 11:53:19 +0600

    updated middleware to prevent unauthorized access of admin panel

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,11 +1,37 @@
-import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import {
+  clerkMiddleware,
+  createRouteMatcher,
+  clerkClient,
+} from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isProtectedRoute = createRouteMatcher(["/add-note(.*)", "/admin(.*)"]);
+const isAdminRoute = createRouteMatcher(["/admin(.*)"]);
 
 export default clerkMiddleware(async (auth, request) => {
   if (isProtectedRoute(request)) {
     await auth.protect();
+
+    const { userId } = await auth();
+
+    if (!userId) {
+      return new Response("Unauthorized", {
+        status: 401,
+      });
+    }
+
+    const client = await clerkClient(); // ← call the function
+    const user = await client.users.getUser(userId);
+    const role = user.publicMetadata?.role;
+
+    if (isAdminRoute(request)) {
+      if (role !== "ADMIN" && role !== "MODERATOR") {
+        return NextResponse.redirect(new URL("/", request.url));
+      }
+    }
   }
+
+  return NextResponse.next();
 });
 
 export const config = {
