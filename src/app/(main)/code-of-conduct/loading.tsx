da774f4c62af83a2a55565f54ae97df83f66e372
Sajid Hossain <bmsajidhossain420@gmail.com>
2025-10-21 16:41:33 +0600

    Loading ui for code-of-conduct page

--- src/app/(main)/code-of-conduct/loading.tsx
+++ src/app/(main)/code-of-conduct/loading.tsx
@@ -1,9 +1,31 @@
-import { Spinner } from "@/components/spinner";
+import { Skeleton } from "@/components/ui/skeleton";
 
-export default function Loading() {
+export default function CodeOfConductPageLoading() {
   return (
-    <div className="md:mt-10 mt-4 md:space-y-8 space-y-5 lg:max-w-[80%] md:max-w-[70%] mx-auto relative mb-10">
-      <Spinner />
+    <div className="flex flex-col items-center h-full gap-3 pt-2">
+      {/* Header Skeleton */}
+      <div className="flex flex-col items-center gap-2 text-center">
+        {/* Title */}
+        <Skeleton className="w-64 h-8 rounded-lg md:h-12 md:w-96 bg-zinc-600" />
+
+        {/* Subtitle */}
+        <Skeleton className="w-48 h-4 rounded-md md:w-72 bg-zinc-600" />
+
+        {/* Icon */}
+        <Skeleton className="w-5 h-5 rounded-full md:w-14 md:h-14 bg-zinc-600" />
+      </div>
+
+      {/* Code of Conduct List Skeleton */}
+      <div className="overflow-y-auto max-h-[65%] space-y-3 [&::-webkit-scrollbar]:w-1.5 [&::-webkit-scrollbar-track]:rounded-[10px] [&::-webkit-scrollbar-thumb]:cursor-pointer [&::-webkit-scrollbar-thumb]:rounded-[10px] [&::-webkit-scrollbar-track]:bg-stone-600 [&::-webkit-scrollbar-thumb]:bg-stone-300 pt-2 w-full px-2">
+        {Array.from({ length: 5 }).map((_, idx) => (
+          <div
+            key={idx}
+            className="flex items-center justify-between gap-2 p-2 bg-zinc-600 rounded-xl animate-pulse"
+          >
+            <div className="flex items-start h-10 gap-3"></div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
